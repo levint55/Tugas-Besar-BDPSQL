610b710eae996199bc8626188e1b4f42d6e6ae0f
Levint <levintabel@gmail.com>
2019-11-23 13:02:44 +0700

    Add Get Function

--- index.php
+++ index.php
@@ -8,10 +8,11 @@
 </head>
 <body>
     <?php 
-        // scan_db();
-        // add_to_db('mahasiswa', 'Jordansaa', 'nilai:PBO', '40');
+        // scan_db('mahasiswa');
+        // add_to_db('mahasiswa', 'ABC', 'nilai:asd', '55');
+        get_db('mahasiswa', 'ABC');
 
-        function scan_db(){
+        function scan_db($table_name){
             // persiapkan curl
             $ch = curl_init(); 
 
@@ -24,7 +25,7 @@
             curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
 
             // set url 
-            curl_setopt($ch, CURLOPT_URL, "http://localhost:8081/mahasiswa/*");
+            curl_setopt($ch, CURLOPT_URL, "http://localhost:8081/$table_name/*");
 
             // return the transfer as a string 
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
@@ -36,40 +37,8 @@
             curl_close($ch);      
 
             // menampilkan hasil curl
-            $datas = json_decode($output);
-
-            $obj = $datas->Row;
-
-            foreach ($obj as $data) {
-                // nama key
-                $key = base64_decode($data->key);
-                echo $key;
-                echo "\r\n";
-
-                // ambil cell
-                $cells = (array)$data->Cell;
-
-                foreach ($cells as $cell) {
-                    // ambil column
-                    $column = (array)$cell;
-
-                    $kolom = base64_decode($column['column']);
-                    $arr_kolom = explode(':',$kolom);
-
-                    // var_dump($arr_kolom);
-                    $cf = $arr_kolom[0];
-                    $cn = $arr_kolom[1];
-
-                    $isi = base64_decode($column["\$"]);
-
-                    echo $cf;
-                    echo " ";
-                    echo $cn;
-                    echo " ";
-                    echo $isi;
-                }
-                echo "\r\n";
-            }
+            json_to_normal($output);
+            
         }
 
         function add_to_db($table_name, $key, $column_name, $value){
@@ -109,6 +78,70 @@
             curl_exec($ch);
             // tutup curl 
             curl_close($ch);
+        }
+
+        function get_db($table_name, $key){
+            // persiapkan curl
+            $ch = curl_init(); 
+
+            // header
+            $request_headers = array();
+            $request_headers[] = 'Accept: application/json';
+            $request_headers[] = 'Content-Type: application/json';
+
+            // set header
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
+
+            // set url 
+            curl_setopt($ch, CURLOPT_URL, "http://localhost:8081/$table_name/$key");
+
+            // return the transfer as a string 
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+            // $output contains the output string 
+            $output = curl_exec($ch);
+
+            // tutup curl 
+            curl_close($ch);      
+
+            json_to_normal($output);
+        }
+
+        function json_to_normal($datas){
+            $datas = json_decode($datas);
+            $obj = $datas->Row;
+
+            foreach ($obj as $data) {
+                // nama key
+                $key = base64_decode($data->key);
+                echo $key;
+                echo ": ";
+
+                // ambil cell
+                $cells = (array)$data->Cell;
+
+                foreach ($cells as $cell) {
+                    // ambil column
+                    $column = (array)$cell;
+
+                    $kolom = base64_decode($column['column']);
+                    $arr_kolom = explode(':',$kolom);
+
+                    // var_dump($arr_kolom);
+                    $cf = $arr_kolom[0];
+                    $cn = $arr_kolom[1];
+
+                    $isi = base64_decode($column["\$"]);
+
+                    echo $cf;
+                    echo " ";
+                    echo $cn;
+                    echo " ";
+                    echo $isi;
+                    echo ", ";
+                }
+                echo "<br>";
+            }
         }
 
         class Record {
