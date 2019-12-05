66538ecfe23955b2559941626d2f2040ecf93c0a
Levint <levintabel@gmail.com>
2019-12-05 13:45:17 +0700

    Delete , Create Table

---
+++ create_table.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // buat kolomnya dalam bentuk array
+        $data1 = array(
+            'name' => 'kolom1',
+        );
+
+        $data2 = array(
+            'name' => 'kolom2',
+        );
+
+        $arr_data = [];
+
+        array_push($arr_data, $data1, $data2);
+
+        // add_table('table3', $arr_data);
+
+        function add_table($table_name, $columns){
+            $table = new Table($table_name, $columns);
+
+            // persiapkan curl
+            $ch = curl_init(); 
+
+            // header
+            $request_headers = array();
+            $request_headers[] = 'Accept: application/json';
+            $request_headers[] = 'Content-Type: application/json';
+
+            // set header
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
+
+            // set url 
+            curl_setopt($ch, CURLOPT_URL, "http://localhost:8081/$table_name/schema");
+
+            // return the transfer as a string 
+            // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($table));
+
+            // $output contains the output string 
+            curl_exec($ch);
+            // tutup curl 
+            curl_close($ch);
+        }
+
+        function json_to_normal($datas){
+            $datas = json_decode($datas);
+            $obj = $datas->Row;
+
+            foreach ($obj as $data) {
+                // nama key
+                $key = base64_decode($data->key);
+                echo $key;
+                echo ": ";
+
+                // ambil cell
+                $cells = (array)$data->Cell;
+
+                foreach ($cells as $cell) {
+                    // ambil column
+                    $column = (array)$cell;
+
+                    $kolom = base64_decode($column['column']);
+                    $arr_kolom = explode(':',$kolom);
+
+                    // var_dump($arr_kolom);
+                    $cf = $arr_kolom[0];
+                    $cn = $arr_kolom[1];
+
+                    $isi = base64_decode($column["\$"]);
+
+                    echo $cf;
+                    echo " ";
+                    echo $cn;
+                    echo " ";
+                    echo $isi;
+                    echo ", ";
+                }
+                echo "<br>";
+            }
+        }
+
+        class Table {
+            public $name;
+            public $ColumnSchema = [];
+
+            function __construct($table_name, $columns) {
+                $this->name = $table_name;
+                $this->ColumnSchema = $columns;
+              }
+        }
+    ?>
+</body>
+</html>
